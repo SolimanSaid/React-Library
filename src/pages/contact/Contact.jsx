382617af308d17e5621dd7fa1131efbddd346657
soliman <soliman.farag86@gmail.com>
2023-05-22 22:10:37 +0300

    react

---
+++ src/pages/contact/Contact.jsx
@@ -0,0 +1,45 @@
+const Contact = () => {
+  return (
+    <section className="contact">
+      <div className="contact-wrapper">
+        <div className="contact-item">
+          <div className="contact-item-icon">
+            <i className="bi bi-house-fill"></i>
+            Address
+          </div>
+          <p className="contact-item-text">Egypt-Cairo-Nasr city </p>
+        </div>
+        <div className="contact-item">
+          <div className="contact-item-icon">
+            <i className="bi bi-telephone-fill"></i>
+            Phone
+          </div>
+          <p className="contact-item-text">01114491730</p>
+        </div>
+        <div className="contact-item">
+          <div className="contact-item-icon">
+            <i className="bi bi-envelope-fill"></i>
+            Email
+          </div>
+          <p className="contact-item-text">soliman.farag86@gmail.com</p>
+        </div>
+      </div>
+      <form onSubmit={(e) => e.preventDefault()} className="contact-form">
+        <h2 className="contact-form-title">Contact Us Form</h2>
+        <div className="contact-input-wrapper">
+          <input type="text" placeholder="Name *" />
+          <input type="text" placeholder="Subject *" />
+          <input type="text" placeholder="Email *" />
+        </div>
+        <textarea
+          placeholder="Your Message *"
+          className="contact-textarea"
+          rows="5"
+        ></textarea>
+        <button className="contact-btn">Send</button>
+      </form>
+    </section>
+  );
+};
+
+export default Contact;
